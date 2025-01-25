739bc608bedfe64a5cf8c2d2a237c9ed3602bcc0
Sebastian Sosa <1sebastian1sosa1@gmail.com>
2025-01-25 16:59:53 -0500

    weaviate ready

--- srtd/__main__.py
+++ srtd/__main__.py
@@ -2,14 +2,22 @@
 
 import sys
 import core
+from dotenv import load_dotenv
+from weaviate_client import WeaviateClient
+import os
+
+load_dotenv()
 
 
 def main(args=None):
     if args and len(args) > 0:
         source_dir = args[0]
     else:
-        print("Please provide a directory as a command line argument. Exiting.")
-        return -1
+        print("Please provide a directory as a command line argument.")
+        # return -1
+    
+    client = WeaviateClient()
+    client.ensure_collection(os.getenv("COLLECTION_NAME"))
 
     core.buildFileTree(source_dir)
 
