5906a6e11da2d58b0e24605660c428620eea9a2c
Herschenglime <herschenglime@gmail.com>
2025-01-25 15:30:26 -0500

    Add metadata from stat

--- srtd/core.py
+++ srtd/core.py
@@ -15,15 +15,19 @@ def buildFileList(source_dir):
 
     file_list = [];
 
+
     # get directory object
-    with os.scandir(source_dir) as entries:
+    with os.scandir(os.path.abspath(source_dir)) as entries:
         for entry in entries:
             print(entry.name)
             # TODO: refine what goes into one of these objects
             file_info = {
                 'path': entry.path,
                 'name': entry.name,
-                'is_file': entry.is_file(),
                 'is_dir': entry.is_dir(),
+
+                # format of metadata field
+                # https://docs.python.org/3/library/os.html#os.stat_result
+                'metadata': entry.stat()
             }
             file_list.append(file_info)
