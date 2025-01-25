84e7d3e1efc1cebf6032cf94b0592bc6a8757f45
Herschenglime <herschenglime@gmail.com>
2025-01-25 18:16:49 -0500

    add arg to print file list from command line

--- srtd/__main__.py
+++ srtd/__main__.py
@@ -11,7 +11,10 @@ def main(args=None):
         print("Please provide a directory as a command line argument. Exiting.")
         return -1
 
-    core.buildFileList(source_dir)
+    file_list = core.buildFileList(source_dir)
+
+    if len(args) > 1 and args[1] == "print":
+        print(file_list)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
