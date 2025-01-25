695c4bf7eecd9a251e43988c61d86642fc7bd0f8
Anthony Thisse <ant@centripetalfusion.com>
2025-01-25 14:02:41 -0500

    change to qml

--- hello_world.py
+++ hello_world.py
@@ -1,32 +1,17 @@
 import sys
-import random
-from PySide6 import QtCore, QtWidgets, QtGui
-
-
-class MyWidget(QtWidgets.QWidget):
-    def __init__(self):
-        super().__init__()
-
-        self.hello = ["Hallo Welt", "Hei maailma", "Hola Mundo", "Привет мир"]
-
-        self.button = QtWidgets.QPushButton("Click me!")
-        self.text = QtWidgets.QLabel("Hello World", alignment=QtCore.Qt.AlignCenter)
-
-        self.layout = QtWidgets.QVBoxLayout(self)
-        self.layout.addWidget(self.text)
-        self.layout.addWidget(self.button)
-
-        self.button.clicked.connect(self.magic)
-
-    @QtCore.Slot()
-    def magic(self):
-        self.text.setText(random.choice(self.hello))
+from PySide6.QtGui import QGuiApplication
+from PySide6.QtQml import QQmlApplicationEngine
 
 if __name__ == "__main__":
-    app = QtWidgets.QApplication([])
+    app = QGuiApplication(sys.argv)
+    engine = QQmlApplicationEngine()
 
-    widget = MyWidget()
-    widget.resize(800, 600)
-    widget.show()
+    # Load QML file from the same directory
+    engine.load("main.qml")
 
-    sys.exit(app.exec())
+    if not engine.rootObjects():
+        sys.exit(-1)
+
+    exit_code = app.exec()
+    del engine
+    sys.exit(exit_code)
