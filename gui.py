297330571763fb7c38e4ef8d63d0dd46da2878b6
Anthony Thisse <ant@centripetalfusion.com>
2025-01-25 18:10:56 -0500

    implement themes

--- gui.py
+++ gui.py
@@ -1,11 +1,11 @@
 from file_explorer import FileExplorer
-from themes.theme import Theme
+from themes import *
 from PySide6.QtWidgets import (
     QApplication,
 )
 
 if __name__ == "__main__":
     app = QApplication([])
-    window = FileExplorer(Theme(app, "forest_green"))
+    window = FileExplorer(Sand(app))
     window.show()
     app.exec()
--- themes/forest_green.py
+++
@@ -1,9 +0,0 @@
-from PySide6.QtWidgets import QApplication
-
-from theme import Theme
-
-class ForestGreen(Theme):
-    def __init__(self, app: QApplication):
-        self.__primary_color = "#d1e7cc"
-        self.__secondary_color = "#62ad52"
-        super().__init__(app)
