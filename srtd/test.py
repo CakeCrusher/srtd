8a85d2f38045fd203dccefb3a3bc33d4f378597c
Herschenglime <herschenglime@gmail.com>
2025-01-25 21:10:58 -0500

    merged requirements into pyproject.toml

--- srtd/test.py
+++ srtd/test.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
 import core
+import filter
 
-print(core.buildDestinationList(["/home/paul/Pictures"]))
+# print(core.buildDestinationList(["/home/paul/Pictures"]))
 # print(core.buildDestinationList(["/home/paul/School"]))
+
+print(filter.getMatches("scree", core.buildFileList("/home/paul/Pictures")))
